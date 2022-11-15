bc11a0cc3b5bcbada7acb045ad5fa21f99208745
Yasmin <bustamiyasmin@gmail.com>
2022-11-15 05:30:46 -0600

    thought js done

---
+++ models/thought.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+const { Thought } = require('.');
+const reactionSchema = require('./reaction');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (date) => timeSince(date),
+        },
+
+        username: {
+            type: String,
+            required: true,
+
+        },
+
+        description: {
+            type: String,
+            minlength: 10,
+            maxlength: 400,
+        },
+
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+        }
+);
+
+thoughtSchema
+.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('thought', thoughtSchema);
+module.exports = Thought;
