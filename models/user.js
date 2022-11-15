1c93c68bedaaf6ab48e3b16cf98b873d901e7e83
Yasmin <bustamiyasmin@gmail.com>
2022-11-15 05:44:57 -0600

    completed user.js

---
+++ models/user.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            
+        },
+
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'thought',
+        },],  
+
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'user',
+        },],  
+
+
+},
+
+{
+    toJSON: {
+        virtuals: true,
+    },
+    id: false,
+}
+);
+
+userSchema
+.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('user', userSchema);
+module.exports = User;
